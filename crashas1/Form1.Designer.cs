78f1059f7476bb88cedf89e3b433915d7f854e41
ignasgeriausias <167857339+ignasgeriausias@users.noreply.github.com>
2024-05-21 22:51:03 +0300

    Add files via upload
    ignas st

---
+++ crashas1/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace crashas1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblMultiplier = new System.Windows.Forms.Label();
+            this.lblBalance = new System.Windows.Forms.Label();
+            this.txtBetAmount = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCashOut = new System.Windows.Forms.Button();
+            this.gameTimer = new System.Windows.Forms.Timer();
+            this.SuspendLayout();
+            // 
+            // lblMultiplier
+            // 
+            this.lblMultiplier.AutoSize = true;
+            this.lblMultiplier.Location = new System.Drawing.Point(12, 9);
+            this.lblMultiplier.Name = "lblMultiplier";
+            this.lblMultiplier.Size = new System.Drawing.Size(53, 13);
+            this.lblMultiplier.TabIndex = 0;
+            this.lblMultiplier.Text = "Multiplier:";
+            // 
+            // lblBalance
+            // 
+            this.lblBalance.AutoSize = true;
+            this.lblBalance.Location = new System.Drawing.Point(12, 40);
+            this.lblBalance.Name = "lblBalance";
+            this.lblBalance.Size = new System.Drawing.Size(49, 13);
+            this.lblBalance.TabIndex = 1;
+            this.lblBalance.Text = "Balance:";
+            // 
+            // txtBetAmount
+            // 
+            this.txtBetAmount.Location = new System.Drawing.Point(15, 70);
+            this.txtBetAmount.Name = "txtBetAmount";
+            this.txtBetAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtBetAmount.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(15, 100);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCashOut
+            // 
+            this.btnCashOut.Location = new System.Drawing.Point(100, 100);
+            this.btnCashOut.Name = "btnCashOut";
+            this.btnCashOut.Size = new System.Drawing.Size(75, 23);
+            this.btnCashOut.TabIndex = 4;
+            this.btnCashOut.Text = "Cash Out";
+            this.btnCashOut.UseVisualStyleBackColor = true;
+            this.btnCashOut.Click += new System.EventHandler(this.btnCashOut_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 450);
+            this.Controls.Add(this.btnCashOut);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtBetAmount);
+            this.Controls.Add(this.lblBalance);
+            this.Controls.Add(this.lblMultiplier);
+            this.Name = "Form1";
+            this.Text = "Crash Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label lblMultiplier;
+        private System.Windows.Forms.Label lblBalance;
+        private System.Windows.Forms.TextBox txtBetAmount;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCashOut;
+        private System.Windows.Forms.Timer gameTimer;
+
+        #endregion
+    }
+}
+
+
---
+++ crashas1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace crashas1
+{
+    public partial class Form1 : Form
+    {
+        private double balance = 1000;
+        private double currentMultiplier = 1.0;
+        private double betAmount = 0;
+        private Random random = new Random();
+        private bool isGameRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblBalance.Text = $"Balance: ${balance}";
+            lblMultiplier.Text = $"Multiplier: x{currentMultiplier}";
+            btnCashOut.Enabled = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtBetAmount.Text, out betAmount) && betAmount > 0 && betAmount <= balance)
+            {
+                balance -= betAmount;
+                lblBalance.Text = $"Balance: ${balance}";
+                currentMultiplier = 1.0;
+                lblMultiplier.Text = $"Multiplier: x{currentMultiplier}";
+                isGameRunning = true;
+                gameTimer.Interval = 200; // Increase the timer interval to slow down the ticks
+                gameTimer.Start();
+                btnCashOut.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Invalid bet amount.");
+            }
+        }
+
+        private void btnCashOut_Click(object sender, EventArgs e)
+        {
+            if (isGameRunning)
+            {
+                gameTimer.Stop();
+                isGameRunning = false;
+                balance += betAmount * currentMultiplier;
+                lblBalance.Text = $"Balance: ${balance}";
+                MessageBox.Show($"You cashed out at x{currentMultiplier:F1} and won ${betAmount * currentMultiplier}!");
+                btnCashOut.Enabled = false;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (isGameRunning)
+            {
+                currentMultiplier += 0.05; // Decrease the increment value to slow down the multiplier increase
+                lblMultiplier.Text = $"Multiplier: x{currentMultiplier:F1}";
+
+                if (random.NextDouble() < 0.05)
+                {
+                    gameTimer.Stop();
+                    isGameRunning = false;
+                    MessageBox.Show($"Game crashed at x{currentMultiplier:F1}. You lost your bet.");
+                    btnCashOut.Enabled = false;
+                }
+            }
+        }
+    }
+}
+
